2a15dea57a03aec1c5867b95dbe5f9ff329ae485
Elias Jurado <ridimdev@gmail.com>
2026-06-20 18:30:08 -0500

    OCR name correction, dedup to 136 cards, badge regeneration, file rename

--- src/domain/config.js
+++
@@ -1,7 +0,0 @@
-export const CONFIG = Object.freeze({
-  itemsPerPage: 30,
-  maxStatValue: 180,
-  maxTotalValue: 720,
-  localStorageKey: 'pokemonCollected',
-  gridColumns: { sm: 1, md: 2, lg: 3, xl: 4, xxl: 5 },
-});
